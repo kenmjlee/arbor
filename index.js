5e63e7b74f3a25b69a307f4e35bbb952c989da8a
Kenneth Lee <kenneth.mj.lee@gmail.com>
2017-12-16 11:14:27 +0800

    Complete Notifying me there are some articles haven't been read. (+1 squashed commit) Squashed commits: [4488be6] Init a bundled webtask.io project. wt create index.js --name=<webtask name>  --bundle wt udpate <webtask name> index.js -- bundle

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-/**
-* @param context {WebtaskContext}
-*/
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
-};
+// ./index.js
+var Express = require('express');
+var Webtask = require('webtask-tools');
+var bodyParser = require('body-parser')
+var app = Express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+require('./routes/reading')(app);
+
+module.exports = Webtask.fromExpress(app);
